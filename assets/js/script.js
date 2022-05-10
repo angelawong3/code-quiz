592e27dfd133d7b19b6a85cf96bcfcbc7e5fc021
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-10 14:35:10 +0100

    add code for button click in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,103 +33,30 @@ const questions = [
     answer: "console log",
   },
 ];
-let questionIndex = 0;
-let timerValue = 10 * questions.length;
-let quizComplete = false;
 
-var boxDiv = document.querySelector("#boxDiv");
-var currentTimer = document.querySelector("#currentTimer");
-var questionsDiv = document.querySelector("#questionsDiv");
-var startQuiz = document.querySelector("#startQuiz");
+// var currentTimer = document.getElementById("currentTimer");
+// var questionsDiv = document.getElementById("questionsDiv");
+var startQuiz = document.getElementById("startQuiz");
+var boxDiv = document.getElementById("boxDiv");
 
-const onLoad = () => {
-  // initialise local storage
-  // check if highscores exists in LS
-  // if false then set highscores to empty array in LS
+const renderQuestion = () => {
+  console.log("render q");
 };
 
-document
-  .getElementById("startQuiz")
-  .addEventListener("click", removeStartSection);
-function removeStartSection() {
-  document.getElementById("questionsDiv").innerHTML = "";
-}
-
-const startTimer = () => {
-  // declare function to execute every 1 sec
-  const countdown = () => {
-    // decrement timer value
-    // if quizComplete is true then stop timer
-    // check if timer reaches 0
-    // if true render game over
-  };
-
-  // setInterval of 1000ms (1s)
+const removeBoxDiv = () => {
+  console.log("remove d");
 };
 
-const validateAnswer = () => {
-  // get answer clicked from user
-  // get the correct answer for question
-  // compare the 2 answers
-  // if incorrect subtract 5 seconds from timerValue
-  // if incorrect render error alert with message and status
-  // if correct render success alert with message and status
-  // set timeout for 500ms and then go to next question
-  // if question is last question set quizComplete to true and then render form
-  // if question is not last question then increment question index and render next question
-};
+const startButtonClicks = () => {
+  console.log("start clicked");
 
-const handleFormSubmit = () => {
-  // get value from input
-  // check if empty then render error alert with message and status
-  // if not empty then create the score object
-  // {
-  //   fullName: "Bob Smith",
-  //   score: 25
-  // }
-  // push score object to LS
-  // render quizCompleteSection
-};
-
-const renderTimerSection = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-};
-
-const renderQuestionSection = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-  // add click event listener on #question-section
-};
-
-const renderGameOver = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-};
-
-const renderAlert = (message, status) => {
-  // use HTML as guide and build in JS
-  // append div to #question-section
-};
-
-const renderForm = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-  // add submit event handler to form
-};
-
-const renderQuizCompleteSection = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-};
-
-const startQuiz = () => {
-  // remove start section
-  // start timer
-  // render timer section
-  // render question section
+  // remove orgininal boxDiv
+  removeBoxDiv();
+  // render questions
+  renderQuestion();
 };
 
 // add event listeners
+startQuiz.addEventListener("click", startButtonClicks);
 // add document on load event listener
 // add start button click event listener
